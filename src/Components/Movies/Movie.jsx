f61d4db24fc3a7b4937250588a8186d7db9116fa
jimmy <trinh.jimmy10@gmail.com>
2022-11-21 15:01:24 +0100

    correction premier exo-revisions

--- src/Components/Movies/Movie.jsx
+++ src/Components/Movies/Movie.jsx
@@ -14,7 +14,31 @@ const Movie = ({movie, index}) => {
         })
     };
 
+    // const swap = (obj, prop1, prop2) => {
+    //     const tmp = obj[prop1];
+    //     obj[prop1] = obj[prop2];
+    //     obj[prop2] = tmp;
+    // }
+    //
+    // const moveUp = () => {
+    //     if (index > 0) {
+    //         swap(movies, index, index - 1);
+    //     }
+    // }
+    //
+    // const moveDown = () => {
+    //     if (index < movies.length - 1) {
+    //         swap(movies, index, index + 1);
+    //     }
+    // }
 
+    const movePosition = (direction, index) => {
+        const newArr = [...movies];
+        const movieAlternateIndex = direction ? index - 1 : index + 1;
+        newArr[movieAlternateIndex] = movies[index];
+        newArr[index] = movies[movieAlternateIndex];
+        setMovies(newArr);
+    }
 
     return (
         <div>
@@ -22,10 +46,13 @@ const Movie = ({movie, index}) => {
             Nom du film : {movie.name}, Date de sortie du film : {movie.year}
             <button onClick={handleClick}>X</button>
             {
-
+                index !== 0 &&
+                <button onClick={() => movePosition(true, index)}>UP</button>
             }
-            {/*<button onClick={changePosition}>UP</button>*/}
-            {/*<button onClick={handleDown}>DOWN</button>*/}
+            {
+                index !== movies.length - 1 &&
+                <button onClick={() => movePosition(false, index)}>DOWN</button>
+            }
         </div>
     );
 };
